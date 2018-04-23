a7490d0ff886a6cb48f1cbf486444864c1c5a3a2
Mihkel Märtin <mihkelmartin@gmail.com>
2018-04-23 13:27:52 +0300

    Uued komponendid tehtud Event lehekülje jaoks

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -7,7 +7,7 @@ const ReactDOM = require('react-dom');
 var $ = require('jquery');
 
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-import {Event} from './event.js';
+import {EventDashBoard} from './event.js';
 
 // tag::app[]
 class App extends React.Component {
@@ -57,15 +57,19 @@ class EventList extends React.Component {
 			<EventElement key={event.id} event={event} onEventSelected = {this.props.onEventSelected}/>
 		);
 		return (
-			<table className="ui collapsing celled table">
-				<tbody>
-					<tr>
-						<th className = "center aligned">Name</th>
-						<th className = "center aligned">PIN</th>
-					</tr>
-					{events}
-				</tbody>
-			</table>
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <table className="ui collapsing celled table">
+                        <tbody>
+                            <tr>
+                                <th className = "center aligned">Name</th>
+                                <th className = "center aligned">PIN</th>
+                            </tr>
+                            {events}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 		)
 	}
 }
@@ -109,9 +113,11 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-		    <div className="icon input">
-                <input type="text" placeholder="Event member e-mail..." value = {this.props.email} onChange={this.onEmailTextChange}/>
-                <i className="search icon"></i>
+            <div className="ui centered header">
+                <div className="icon input">
+                    <input type="text" placeholder="Event member e-mail..." value = {this.props.email} onChange={this.onEmailTextChange}/>
+                    <i className="search icon"></i>
+                </div>
             </div>
         )
 	}
@@ -137,7 +143,7 @@ class Main extends React.Component {
             <BrowserRouter>
                 <Switch>
                   <Route exact path='/' render={(props) => <App{...props} onEventSelected = {this.handleEventSelected}/>}/>
-                  <Route path='/Event' render={(props) => <Event{...props} eventId = {this.state.selectedEvent}/>}/>
+                  <Route path='/Event' render={(props) => <EventDashBoard{...props} eventId = {this.state.selectedEvent}/>}/>
                 </Switch>
             </BrowserRouter>
         )
--- src/main/js/event.js
+++ src/main/js/event.js
@@ -3,7 +3,28 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 var $ = require('jquery');
 
-// tag::Event[]
+// tag::EventDashBoard[]
+export class EventDashBoard extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+	render() {
+		return (
+		    <div>
+		        <Event eventId = {this.props.eventId}/>
+                <div className="ui divider"></div>
+                <MemberList/>
+                <div className="ui divider"></div>
+                <TransactionList/>
+                <div className="ui divider"></div>
+		    </div>
+		)
+	}
+}
+// end::EventDashBoard[]
+
 export class Event extends React.Component {
 
 	constructor(props) {
@@ -11,13 +32,93 @@ export class Event extends React.Component {
 
 	}
 	render() {
-	        console.log(this.props.eventId);
-
 		return (
-		    <p>Siia tuleb Event suurlehekülg. Event {this.props.eventId}</p>
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='header'>
+                      <input type="text" defaultvalue = {this.props.eventId} onChange={this.onEmailTextChange}/>
+                    </div>
+                    <div className='extra content'>
+                        <span className='right floated edit icon'>
+                            <i className='edit icon' />
+                        </span>
+                        <span className='right floated trash icon'>
+                            <i className='trash icon' />
+                        </span>
+                    </div>
+                </div>
+            </div>
 		)
 	}
 }
-// end::Event[]
 
+export class MemberList extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+	render() {
+		return (
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <Member/>
+                    <div className='ui basic content center aligned segment'>
+                        <button className='ui basic button icon' onClick={this.handleFormOpen}>
+                            <i className='plus icon' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+		)
+	}
+}
+
+export class Member extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+	render() {
+		return (
+                <p>Mihkel Märtin Miku</p>
+		)
+	}
+}
+
+export class TransactionList extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+	render() {
+		return (
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <Transaction/>
+                    <div className='ui basic content center aligned segment'>
+                        <button className='ui basic button icon' onClick={this.handleFormOpen}>
+                            <i className='plus icon' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+		)
+	}
+}
+
+export class Transaction extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+	}
+	render() {
+		return (
+                <p>Esimene poeskäik</p>
+		)
+	}
+}
 
