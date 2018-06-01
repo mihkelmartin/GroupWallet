e151ace84c54b15b65e970f70b307de8380ca7f9
Mihkel Märtin <mihkelmartin@gmail.com>
2018-06-01 14:34:30 +0300

    JavaMailSenderi autokonfigureerimine ei paista töötavat.

--- backend/src/main/java/model/AppConfig.java
+++ backend/src/main/java/model/AppConfig.java
@@ -4,15 +4,21 @@ import aspects.MoneyCalculationAspect;
 import com.mongodb.MongoClient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import repository.*;
 import service.*;
+
+import java.util.Properties;
 
 
 /**
@@ -20,6 +26,7 @@ import service.*;
  */
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScan("service")
 public class AppConfig {
 
     @Value( "${mongoDB.mongoHost}" )
@@ -37,6 +44,32 @@ public class AppConfig {
     public @Bean
     MongoOperations mongoOperations() throws Exception {
         return new MongoTemplate(mongoDbSimpleFactory());
+    }
+
+    public @Bean
+    JavaMailSender javaMailSender() throws Exception {
+
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(587);
+        sender.setUsername("walletforteam@gmail.com");
+        sender.setPassword("murakas301polaarjoon");
+        sender.setProtocol("smtp");
+
+        Properties mailProps = new Properties();
+        mailProps.put("mail.smtp.port", "25");
+        mailProps.put("mail.smtps.auth", "true");
+        mailProps.put("mail.smtp.starttls.enable", "true");
+        mailProps.put("mail.smtp.starttls.required", "true");
+
+        sender.setJavaMailProperties(mailProps);
+
+        return sender;
+    }
+
+    public @Bean
+    RestTemplateBuilder restTemplateBuilder() throws Exception {
+        return new RestTemplateBuilder();
     }
 
     public @Bean
