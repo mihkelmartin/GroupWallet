dde5333c8990988c1a559fc98b8009319348b94d
Mihkel Märtin <mihkelmartin@gmail.com>
2018-06-15 09:06:03 +0300

    Tegin PIN ja PUK logimise ning vale PINi vea näitamise selliseks nagu vaja.

--- frontend/src/main/js/components/eventelement.js
+++ frontend/src/main/js/components/eventelement.js
@@ -3,18 +3,104 @@
 // tag::vars[]
 const React = require('react');
 
+var $ = require('jquery');
+import {getBackEndUrl} from './getProperties';
+
+import ReCAPTCHA from 'react-google-recaptcha'
+
 // tag::EventElement[]
 class EventElement extends React.Component{
 
+	constructor(props) {
+        super(props);
+		this.puk = -1;
+	}
+
+    state = {pin : '', loginError : false};
+
+    onPINLogin = (token) => {
+        if(this.state.pin){
+            var url = getBackEndUrl() + 'login/' + this.props.event.id + '/' + this.refs.EventPIN.value + '/' + token;
+            console.log(url);
+            $.ajax({
+                url: url,
+                dataType: 'text',
+                cache: false,
+                success: function(data) {
+                    if(data === ""){
+                        this.setState({pin : '', loginError : true});
+                        this.puk = -1;
+                    }
+                    else
+                        this.props.onEventSelected(this.props.event.id, data);
+              }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error(err.toString());
+                }.bind(this)
+            });
+        }
+        this.captcha.reset();
+    }
+
+    onPUKLogin = (token) => {
+        if(this.puk !== -1){
+            var url = getBackEndUrl() + 'puk/' + this.props.event.id + '/' + this.puk + '/' + token;
+            console.log(url);
+            $.ajax({
+                url: url,
+                dataType: 'text',
+                cache: false,
+                success: function(data) {
+                    this.puk = -1;
+              }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error(err.toString());
+                }.bind(this)
+            });
+        }
+        this.captcha.reset();
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.onSubmit(e.target.name, this.props.event.id, this.refs.EventPIN.value);
+        this.loginType = e.target.name;
+        this.captcha.execute();
+    }
+
+    onChange = (token) => {
+        if(this.loginType === 'PIN')
+            this.onPINLogin(token);
+        if(this.loginType === 'PUK')
+            this.onPUKLogin(token);
+    }
+
+    onPinChange = (e) => {
+        this.puk = e.target.value;
+        this.setState({pin : e.target.value});
+    }
+
+    onPinBlur = (e) => {
+        this.setState({pin : '', loginError : false});
+    }
+
+    onPinFocus = (e) => {
+        this.puk = -1;
     }
 
 	render() {
-        var PINError;
-        if(this.props.event.id === this.props.loginFailedEventID){
-            PINError =  <div className="floating ui red label">Incorrect PIN!</div>;
+        var PINInput;
+        if(this.state.loginError === false){
+            PINInput =  <div className = "ui fluid input">
+                            <input ref="EventPIN" type="number" placeholder="Enter PIN and press ENTER"
+                                value={this.state.pin} onChange={this.onPinChange} onBlur={this.onPinBlur}
+                                    onFocus={this.onPinFocus}/>
+                        </div>;
+        } else {
+            PINInput =  <div className = "ui fluid error input">
+                            <input ref="EventPIN" type="number" placeholder="Incorrect PIN"
+                                value={this.state.pin} onChange={this.onPinChange} onBlur={this.onPinBlur}
+                                    onFocus={this.onPinFocus}/>
+                        </div>;
         }
 
 		return (
@@ -27,12 +113,15 @@ class EventElement extends React.Component{
                 </div>
                 <div className = "three wide column middle aligned">
                     <form name="PIN" onSubmit={this.onSubmit}>
-                    <div className = "ui fluid input">
-                        {PINError}
-                        <input ref="EventPIN" type="number" placeholder="Enter PIN and press ENTER"/>
-                    </div>
+                        {PINInput}
                     </form>
 				</div>
+                <ReCAPTCHA
+                  ref={(el) => { this.captcha = el; }}
+                  size="invisible"
+                  sitekey="6LdhaFwUAAAAAIorazYNYgkIr_sps2S2hrdMDxRa"
+                  onChange={this.onChange}
+                />
 			</div>
 		)
 	}
